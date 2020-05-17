7f71a582a628ea20e12b37f3f629e82e3b2c146f
Saharsh Yeruva <hello@saharsh.tech>
2020-05-17 23:21:12 +0000

    Added proper .env variables. BOT IS WORKING

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-wconst mineflayer = require('mineflayer')
-// if (process.argv.length < 4 || process.argv.length > 6) {
-//     console.log('Usage : node process.env.FILE_PATH process.env.HOST process.env.PORT process.env.NAME process.env.PASSWORD')
-//     process.exit(1)
-//   }
-
+const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-  host: process.argv[2],
-  port: parseInt(process.argv[3]),
-  username: process.argv[4] ? process.argv[4] : 'repl',
-  password: process.argv[5]
+  host: process.env.HOST,
+  port: parseInt(process.env.PORT),
+  username: process.env.NAME ? process.env.NAME : 'index',
+  password: process.env.PASSWORD
 })
