192da540591a92c4452491ead94597a7d5534039
Saharsh Yeruva <hello@saharsh.tech>
2020-05-17 23:34:19 +0000

    Added URL for Uptime Robot to ping

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const mineflayer = require('mineflayer')
+const express = require("express")
+const app = express()
+
+app.get("/",(req,res) => res.send("I'm awake bro."))
 
 const bot = mineflayer.createBot({
   host: process.env.HOST,
@@ -6,3 +10,5 @@ const bot = mineflayer.createBot({
   username: process.env.NAME ? process.env.NAME : 'index',
   password: process.env.PASSWORD
 })
+
+app.listen(3000, () => console.log("I'm on 3000"))
