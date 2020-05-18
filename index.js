3e3e1988bf04321e26ca0a727e2cfe9837170e70
Saharsh Yeruva <hello@saharsh.tech>
2020-05-18 05:09:40 +0000

    Bot leaves when whispered to

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ const mineflayer = require('mineflayer')
 const express = require("express")
 const app = express()
 
-app.get("/",(req,res) => res.send("I'm awake."))
+console.log("starting bot"); //A heads-up that the bot is starting up
+
+app.get("/", (req, res) => res.send("I'm awake."))
 
 const bot = mineflayer.createBot({
-  host: process.env.HOST,
-  port: parseInt(process.env.PORT),
-  username: process.env.NAME ? process.env.NAME : 'index',
-  password: process.env.PASSWORD,
-	
-	keepAlive: true,
-	checkTimeoutInterval: 30*1000
+	host: process.env.HOST,
+	port: parseInt(process.env.PORT),
+	username: process.env.NAME ? process.env.NAME : 'index',
+	password: process.env.PASSWORD,
 })
 
+// Initial things when bot joins the server
 bot.on('login', () => {
-    bot.chat('Hey, I’m Saharsh’s bot! I’ll be AFKing for a bit.')
-		bot.chat('/nick Booomerr (bot)')
-    bot.chat('/afk')
-		
+	bot.chat('Hey, I’m Saharsh’s bot! I’ll be AFKing for a bit.') 	// Message to let players to about the bot
+	bot.chat('/nick Booomerr (bot)') // Setting nickname to make it clear that it's a bot
+	bot.chat('/afk') // Sets the bot to AFK
 })
 
-bot.on('error', err => {
-    console.log(err)
-})
+// Bot will quit when whispered to
+bot.on('whisper', function(username, message) {
+	if (username === bot.username) return;
+	bot.quit();
+});
 
 app.listen(3000, () => console.log("I'm on 3000"))
