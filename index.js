ba6c71d9b3eb35a22d4b80e4c5bf7093eaf29489
Saharsh Yeruva <hello@saharsh.tech>
2020-06-01 03:04:07 +0000

    Attempting come command

--- index.js
+++ index.js
@@ -11,23 +11,40 @@ const express = require("express")
 const app = express()
 const chalk = require("chalk");
 const v = require("vec3");
-const mineflayer = require('mineflayer');
 const navigatePlugin = require('mineflayer-navigate')(mineflayer);
-const bot = mineflayer.createBot({ username: 'Player' });
-navigatePlugin(bot);
+
 
 app.get("/", (req, res) => res.send(""))
 
 console.log(chalk.green("Bot is starting")); //A heads-up that the bot is starting
 
+// Creates bot
 const bot = mineflayer.createBot({
 	host: process.env.HOST,
 	port: parseInt(process.env.PORT),
 	username: process.env.NAME ? process.env.NAME : 'index',
 	password: process.env.PASSWORD,
 })
+navigatePlugin(bot); // For bot navigation
 
 var eternumMembers = ['ExpressCow29', 'ExpressCow30', 'Nice6599', 'Nice6099'];
+
+bot.navigate.on('pathFound', function(path) {
+		bot.chat("/msg " + username + " " + "found path. I can get there in " + path.length + " moves.");
+	});
+
+	bot.navigate.on('cannotFind', function(closestPath) {
+		bot.chat("/msg " + username + " " + "unable to find path. getting as close as possible");
+		bot.navigate.walk(closestPath);
+	});
+
+	bot.navigate.on('arrived', function() {
+		bot.chat("/msg " + username + " " + "I have arrived");
+	});
+
+	bot.navigate.on('interrupted', function() {
+		bot.chat("/msg " + username + " " + "stopping");
+	});
 
 // Initial things when bot joins the server
 bot.on('spawn', () => {
@@ -42,14 +59,23 @@ bot.on('spawn', () => {
 // Commands that only EC29 can run
 bot.on('whisper', function(username, message) {
 	if (username == bot.username) return; // Checks to make sure that the bot isn't whispering to itself
-	
+
+	const target = bot.players[username].entity;
+
+	if (message === 'come') {
+		bot.navigate.to(target.position);
+	}
+
+	if (message === 'stop') {
+		bot.navigate.stop();
+	}
+
 	// Commands Exclusive to Eternum Members
 	if (eternumMembers.includes(username)) {
-		if(message.includes("leave")) {
-		console.log(chalk.red(username + "said to leave"));
-		quitGame();
+		if (message === 'leave') {
+			console.log(chalk.red(username + "said to leave"));
+			quitGame();
 		}
-
 
 	}
 
@@ -78,6 +104,36 @@ function currentPlayers(action) {
 function quitGame() {
 	bot.quit();
 	console.log(chalk.red("Bot left the server")) // Making it clear that the bot left
+}
+
+function navigate() {
+
+	bot.navigate.on('pathFound', function(path) {
+		bot.chat("/msg " + username + " " + "found path. I can get there in " + path.length + " moves.");
+	});
+
+	bot.navigate.on('cannotFind', function(closestPath) {
+		bot.chat("/msg " + username + " " + "unable to find path. getting as close as possible");
+		bot.navigate.walk(closestPath);
+	});
+
+	bot.navigate.on('arrived', function() {
+		bot.chat("/msg " + username + " " + "I have arrived");
+	});
+
+	bot.navigate.on('interrupted', function() {
+		bot.chat("/msg " + username + " " + "stopping");
+	});
+
+	const target = bot.players[username].entity;
+
+	if (message === 'come') {
+		bot.navigate.to(target.position);
+	}
+
+	if (message === 'stop') {
+		bot.navigate.stop();
+	}
 }
 
 bot.on('playerLeft', function(player) {
