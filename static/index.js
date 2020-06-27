f7261b2550bcd3223e6d68009f27f33e76818348
Saharsh Yeruva <syeruva2003@gmail.com>
2020-06-27 19:56:17 -0400

    Added switches to bot and websocket
    The switches still don't have functionality
    Now will call the "toggles" "switches"

--- static/index.js
+++ static/index.js
@@ -1,44 +1,44 @@
 /// WebSocket And stuff
-const url = "http://0.0.0.0";
+const url = 'http://0.0.0.0';
 var connected = false;
 var keys = { w: false, a: false, s: false, d: false, space: false };
 
 // listeners
-document.getElementById("botSelected").onchange = function (event) {
+document.getElementById('botSelected').onchange = function (event) {
   console.log(getCurrentBot());
 };
 
 document.addEventListener(
-  "keydown",
+  'keydown',
   (event) => {
-    var key = "";
+    var key = '';
     const keyName = event.code;
     switch (keyName) {
-      case "KeyW":
+      case 'KeyW':
         keys.w = true;
-        key = "forward";
+        key = 'forward';
         break;
-      case "KeyA":
+      case 'KeyA':
         keys.a = true;
-        key = "right";
+        key = 'right';
         break;
-      case "KeyS":
+      case 'KeyS':
         keys.s = true;
-        key = "back";
+        key = 'back';
         break;
-      case "KeyD":
+      case 'KeyD':
         keys.d = true;
-        key = "left";
+        key = 'left';
         break;
-      case "Space":
+      case 'Space':
         keys.space = true;
-        key = "jump";
+        key = 'jump';
         break;
     }
-    if (key != "") {
+    if (key != '') {
       keylogger(keys);
       send({
-        action: "move",
+        action: 'move',
         botId: getCurrentBot(),
         data: { operation: key, state: true },
       });
@@ -48,38 +48,38 @@ document.addEventListener(
 );
 
 document.addEventListener(
-  "keyup",
+  'keyup',
   (event) => {
     if (event.repeat) {
       return;
     }
     const keyName = event.code;
     switch (keyName) {
-      case "KeyW":
+      case 'KeyW':
         keys.w = false;
-        key = "forward";
+        key = 'forward';
         break;
-      case "KeyA":
+      case 'KeyA':
         keys.a = false;
-        key = "right";
+        key = 'right';
         break;
-      case "KeyS":
+      case 'KeyS':
         keys.s = false;
-        key = "back";
+        key = 'back';
         break;
-      case "KeyD":
+      case 'KeyD':
         keys.d = false;
-        key = "left";
+        key = 'left';
         break;
-      case "Space":
+      case 'Space':
         keys.space = false;
-        key = "jump";
+        key = 'jump';
         break;
     }
-    if (key != "") {
+    if (key != '') {
       keylogger(keys);
       send({
-        action: "move",
+        action: 'move',
         botId: getCurrentBot(),
         data: { operation: key, state: false },
       });
@@ -91,7 +91,7 @@ document.addEventListener(
 // looks like a function but is actually just to trigger the listeners after the server is online
 function listenSocket() {
   socketserver.onopen = function (event) {
-    console.log("connected");
+    console.log('connected');
     connected = true;
     serverOnline(true);
   };
@@ -99,22 +99,22 @@ function listenSocket() {
     console.log(event.data);
     var message = JSON.parse(event.data);
     switch (message.action) {
-      case "coords":
+      case 'coords':
         setCoords(message);
         break;
-      case "health":
+      case 'health':
         setHealth(message);
         break;
-      case "hunger":
+      case 'hunger':
         setHunger(message);
         break;
-      case "started":
+      case 'started':
         botOnline(message.action);
         break;
     }
   };
   socketserver.onclose = function (event) {
-    console.log("connection closed");
+    console.log('connection closed');
     serverOnline(false);
   };
 }
@@ -123,57 +123,57 @@ function listenSocket() {
 
 function say(message) {
   send({
-    action: "say",
+    action: 'say',
     botId: getCurrentBot(),
     message: message,
   });
 }
 function kill() {
   send({
-    action: "kill",
+    action: 'kill',
     botId: getCurrentBot(),
   });
 }
 
 function place() {
   send({
-    action: "place",
+    action: 'place',
     botId: getCurrentBot(),
   });
 }
 function attack() {
   send({
-    action: "attack",
+    action: 'attack',
     botId: getCurrentBot(),
   });
 }
 function setServer(address, port) {
-  var address = document.getElementById("server").value;
-  var port = document.getElementById("port").value;
+  var address = document.getElementById('server').value;
+  var port = document.getElementById('port').value;
   var data = { address: address, port: port };
 
   send({
-    action: "setServer",
+    action: 'setServer',
     botId: getCurrentBot(),
     data: data,
   });
 }
 function stopBot() {
   send({
-    action: "stop",
+    action: 'stop',
     botId: getCurrentBot(),
   });
 }
 function startBot() {
   send({
-    action: "start",
+    action: 'start',
     botId: getCurrentBot(),
   });
 }
 function getCoords() {
   if (connected) {
     send({
-      action: "getCoords",
+      action: 'getCoords',
       botId: getCurrentBot(),
     });
   }
@@ -181,7 +181,7 @@ function getCoords() {
 function getHealth() {
   if (connected) {
     send({
-      action: "getHealth",
+      action: 'getHealth',
       botId: getCurrentBot(),
     });
   }
@@ -189,30 +189,30 @@ function getHealth() {
 function getHunger() {
   if (connected) {
     send({
-      action: "getHunger",
+      action: 'getHunger',
       botId: getCurrentBot(),
     });
   }
 }
 
 function setHunger(message) {
-  document.getElementById("hunger").innerHTML = message.data;
+  document.getElementById('hunger').innerHTML = message.data;
 }
 
 function setCoords(message) {
-  document.getElementById("coords").innerHTML =
+  document.getElementById('coords').innerHTML =
     Math.round(message.data.x) +
-    " " +
+    ' ' +
     Math.round(message.data.y) +
-    " " +
+    ' ' +
     Math.round(message.data.z);
 }
 function setHealth(message) {
-  document.getElementById("health").innerHTML = message.data;
+  document.getElementById('health').innerHTML = message.data;
 }
 
 function getCurrentBot() {
-  return document.getElementById("botSelected").value;
+  return document.getElementById('botSelected').value;
 }
 
 /*
@@ -230,65 +230,65 @@ function closeWebSocket() {
   socketserver.close();
 }
 function startWebSocket() {
-  socketserver = new WebSocket("ws://0.0.0.0:3000", "protocolOne");
+  socketserver = new WebSocket('ws://0.0.0.0:3000', 'protocolOne');
   listenSocket();
 }
 
 function toggleTheme() {
-  document.body.classList.toggle("dark-body");
+  document.body.classList.toggle('dark-body');
   for (
     let index = 0;
-    index < document.getElementsByClassName("card").length;
+    index < document.getElementsByClassName('card').length;
     index++
   ) {
     document
-      .getElementsByClassName("card")
+      .getElementsByClassName('card')
       .item(index)
-      .classList.toggle("bg-dark");
+      .classList.toggle('bg-dark');
   }
 }
 
 function serverOnline(state) {
-  var status = document.getElementById("serverStatus");
+  var status = document.getElementById('serverStatus');
   if (state) {
-    status.setAttribute("class", "badge badge-success");
-    status.innerHTML = "Online";
+    status.setAttribute('class', 'badge badge-success');
+    status.innerHTML = 'Online';
   } else {
-    status.setAttribute("class", "badge badge-danger");
-    status.innerHTML = "Offline";
+    status.setAttribute('class', 'badge badge-danger');
+    status.innerHTML = 'Offline';
   }
 }
 function botOnline(state) {
-  var status = document.getElementById("botStatus");
+  var status = document.getElementById('botStatus');
 
   switch (state) {
-    case "started":
-      status.setAttribute("class", "badge badge-success");
-      status.innerHTML = "Connected";
+    case 'started':
+      status.setAttribute('class', 'badge badge-success');
+      status.innerHTML = 'Connected';
       break;
-    case "stopped":
-      status.setAttribute("class", "badge badge-danger");
+    case 'stopped':
+      status.setAttribute('class', 'badge badge-danger');
       break;
 
-    case "starting":
-      status.setAttribute("class", "badge badge-primary");
-      status.innerHTML = "starting";
+    case 'starting':
+      status.setAttribute('class', 'badge badge-primary');
+      status.innerHTML = 'starting';
   }
 }
 
 function keylogger(keys) {
-  if (keys.w) document.getElementById("w").classList.add("keypressed");
-  if (keys.a) document.getElementById("a").classList.add("keypressed");
-  if (keys.s) document.getElementById("s").classList.add("keypressed");
-  if (keys.d) document.getElementById("d").classList.add("keypressed");
+  if (keys.w) document.getElementById('w').classList.add('keypressed');
+  if (keys.a) document.getElementById('a').classList.add('keypressed');
+  if (keys.s) document.getElementById('s').classList.add('keypressed');
+  if (keys.d) document.getElementById('d').classList.add('keypressed');
   if (keys.space)
-    document.getElementById("spacebar").classList.add("keypressed");
-  if (!keys.w) document.getElementById("w").classList.remove("keypressed");
-  if (!keys.a) document.getElementById("a").classList.remove("keypressed");
-  if (!keys.s) document.getElementById("s").classList.remove("keypressed");
-  if (!keys.d) document.getElementById("d").classList.remove("keypressed");
+    document.getElementById('spacebar').classList.add('keypressed');
+  if (!keys.w) document.getElementById('w').classList.remove('keypressed');
+  if (!keys.a) document.getElementById('a').classList.remove('keypressed');
+  if (!keys.s) document.getElementById('s').classList.remove('keypressed');
+  if (!keys.d) document.getElementById('d').classList.remove('keypressed');
   if (!keys.space)
-    document.getElementById("spacebar").classList.remove("keypressed");
+    document.getElementById('spacebar').classList.remove('keypressed');
 }
 
 setTimeout(() => startWebSocket(), 100);
